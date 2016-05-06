7e4207e8e7f836b3b09c80f575bc4531932cd59b
Michael Batz <mail@michael-batz.de>
2016-05-06 17:39:03 +0200

    make receiver and forwarder classes configurable

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from mods.receiver.SnmpTrapReceiver import SnmpTrapReceiver
 from mods.scheduler.Scheduler import Scheduler
 from mods.config.Config import Config
 
-config = Config("./etc/config.ini")
-#forwarder = SmsEagleForwarder(config)
-forwarder = StdoutForwarder(config)
-scheduler = Scheduler(config, forwarder)
-receiver = SnmpTrapReceiver(config, scheduler)
 
+# get configuration
+config = Config("./etc/config.ini")
+classNameForwarder = config.getValue('general', 'fowarder', 'StdoutForwarder')
+classNameReceiver = config.getValue('general', 'fowarder', 'SnmpTrapReceiver')
+
+# create objects
+forwarder = eval(classNameForwarder + "(config)")
+scheduler = Scheduler(config, forwarder)
+receiver = eval(classNameReceiver + "(config, scheduler)")
+
+# start threads
 scheduler.start()
 receiver.start()
 
