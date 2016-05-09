085f055cdb2f90fe3d7e0f50ea6fa9be05dfb346
Michael Batz <mail@michael-batz.de>
2016-05-09 11:20:55 +0200

    make config class thread safe

--- mods/config/Config.py
+++ mods/config/Config.py
@@ -1,22 +1,33 @@
 import configparser
+import threading
 
 class Config:
 
     def __init__(self, filename):
+        self.lock = threading.RLock()
+        self.lock.acquire()
         self.filename = filename
         self.config = configparser.ConfigParser()
         self.config.read(filename)
+        self.lock.release()
 
     def getValue(self, sectionName, key, defaultValue):
+        # set default value
         output = defaultValue
+
+        # get value from config
+        self.lock.acquire()
         try:
             output = self.config[sectionName][key]
         except:
             pass
+        self.lock.release()
 
+        # return value
         return output
 
     def setValue(self, sectionName, key, value):
+        self.lock.acquire()
         # set value in data structure
         try:
             self.config[sectionName]
@@ -27,3 +38,4 @@ class Config:
         # save configuration file
         with open(self.filename, 'w') as configfile:
             self.config.write(configfile)
+        self.lock.release()
